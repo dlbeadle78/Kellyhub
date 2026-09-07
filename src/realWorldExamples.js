bb3de8685c7600748ac8745799f28e7960bf0666
dlbeadle78 <dlbeadle@icloud.com>
2026-09-07 07:43:00 +0100

    Wire researched History examples into learning cards

--- src/realWorldExamples.js
+++ src/realWorldExamples.js
@@ -1,11 +1,13 @@
 import {examplesFor as legacyExamplesFor} from './realWorldExamplesLegacy.js'
 import {LAW_REAL_WORLD_EXAMPLES} from './lawRealWorldExamples.js'
 import {SOCIOLOGY_REAL_WORLD_EXAMPLES} from './sociologyRealWorldExamples.js'
+import {HISTORY_REAL_WORLD_EXAMPLES} from './historyRealWorldExamples.js'
 
 export function examplesFor(subjectSlug, topicSlug) {
   const lawRows = subjectSlug === 'law' ? LAW_REAL_WORLD_EXAMPLES[topicSlug] : null
   const sociologyRows = subjectSlug === 'sociology' ? SOCIOLOGY_REAL_WORLD_EXAMPLES[topicSlug] : null
-  return lawRows || sociologyRows || legacyExamplesFor(subjectSlug, topicSlug)
+  const historyRows = subjectSlug === 'history' ? HISTORY_REAL_WORLD_EXAMPLES[topicSlug] : null
+  return lawRows || sociologyRows || historyRows || legacyExamplesFor(subjectSlug, topicSlug)
 }
 
 export function exampleFor(subjectSlug, topicSlug, index = 0) {
